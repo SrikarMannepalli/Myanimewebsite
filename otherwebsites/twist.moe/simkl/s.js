89850a4ec6b5e691abbdc2d579c90d8f0ab9bc28
phanirithvij <phanirithvij20003@gmail.com>
2018-03-11 22:34:49 +0530

    DAIJOBU

---
+++ otherwebsites/twist.moe/simkl/s.js
@@ -0,0 +1,4 @@
+var cons = c.filter((v)=>{
+	return v.type == "tv"
+});
+console.log(cons);
--- otherwebsites/twist.moe/simkl/searchByText.sh
+++ otherwebsites/twist.moe/simkl/searchByText.sh
@@ -1,5 +1,14 @@
 text=$1
-if [ ]
+#IF type is specified
+if [ $# -eq 1 ]
 then
-typ=$2
-url="https://api.simkl.com/search/movie?q=$text&client_id=f62476556cc7cd5f05720424885a878c71df4f859cef9c15bbe9df862b608521"
+	typ='anime'
+else
+	typ=$2
+fi
+
+url="https://api.simkl.com/search/$typ?q=$text&client_id=f62476556cc7cd5f05720424885a878c71df4f859cef9c15bbe9df862b608521"
+
+wget -q -O h.json $url
+cat h.json | python -m json.tool > outjson.json 
+rm -f h.json
